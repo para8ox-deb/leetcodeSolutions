066eadced57ad6a33f5d20cde62ca6bf2cb2cb11
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-09-14 11:51:25 +0530

    Time: 91 ms (78.68%), Space: 60 MB (13.01%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -1,19 +1,49 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int maxx=0;
 
-        for(int i=0;i<arr.size();i++){
-            maxx=max(maxx,arr[i]);
-        }
+        // Fast approach
 
-        for(int i=0;i<arr.size();i++){
-            if(maxx==arr[i]){
-                return i;
+        int n=arr.size();
+        int s=0;
+        int e=n-1;
+
+        int mid;
+
+        while(s<e){
+            mid=s+(e-s)/2;
+            if(arr[mid]<arr[mid+1]){
+                s=mid+1;
             }
+            else{
+                e=mid;
+            }
+
+            
         }
 
-        return 0;
+        return s;
+
+
+
+
+
+
+
+        // O(n^2) - method
+        // int maxx=0;
+
+        // for(int i=0;i<arr.size();i++){
+        //     maxx=max(maxx,arr[i]);
+        // }
+
+        // for(int i=0;i<arr.size();i++){
+        //     if(maxx==arr[i]){
+        //         return i;
+        //     }
+        // }
+
+        // return 0;
 
     }
 };
