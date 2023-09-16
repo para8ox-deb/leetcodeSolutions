38d22033ef88cbe3a9ea83fc7cf97d6a814169c2
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-09-16 16:52:06 +0530

    Time: 3 ms (70.45%), Space: 9.1 MB (21.56%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int maxx=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            maxx=max(nums[i],maxx);
-        }
 
-        for(int i=0;i<nums.size();i++){
-            if(maxx==nums[i]){
-                return i;
+
+        // Better approach
+
+        int s=0;
+        int e=nums.size()-1;
+
+        int mid = s+(e-s)/2;
+
+        while(s<e){
+            mid = s+(e-s)/2;
+            if(nums[mid]<nums[mid+1]){
+                // Means we are in A side   - we'll go to right side
+                s=mid+1;
+            }
+            else{
+                // It means either we're on peak ele or in B side - we'll go left
+                e=mid;  //e=mid-1; not used coz if we do that then we might lose peak element
             }
         }
+        
+        return s;   //return e will also work at last both will be at same only
+        
 
-        return 0;
+
+
+
+        // Working but TIME COMPLEXITY - O(n)
+
+        // int maxx=INT_MIN;
+        // for(int i=0;i<nums.size();i++){
+        //     maxx=max(nums[i],maxx);
+        // }
+
+        // for(int i=0;i<nums.size();i++){
+        //     if(maxx==nums[i]){
+        //         return i;
+        //     }
+        // }
+
+        // return 0;
     }
 };
