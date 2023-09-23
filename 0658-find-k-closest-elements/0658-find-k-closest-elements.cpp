6497c84f15e88dcb9eaf884c124446361c126ee1
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-09-23 18:50:35 +0530

    Time: 22 ms (96.19%), Space: 31.4 MB (71.31%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,21 +1,71 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        
-        // Two pointer approach - O(n-k)
 
-        int l=0;
-        int h=arr.size()-1;
+    int lowerBound(vector<int>& arr, int x){
+        int s=0;
+        int e=arr.size()-1;
+        int ans=arr.size()-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
 
-        while(h-l>=k){
-            if(x-arr[l]>arr[h]-x){
-                l++;
+            if(arr[mid]>=x){
+                // means arr[mid] is either greater or equal to x
+                ans=mid;
+                e=mid-1;
+            }
+            else if(x>arr[mid]){
+                // move right
+                s=mid+1;
             }
             else{
-                h--;
+                e=mid-1;
             }
         }
 
-        return vector<int>(arr.begin()+l,arr.begin()+h+1);
+        return ans;
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        
+
+        // Binary search and 2 pointer approach - Bottom to top approach (small to big window)
+        int h = lowerBound(arr,x);
+        int l=h-1;
+        // cout<<h;
+        // here we will expand the window [l,h] to k
+
+        while(k--){
+            if(l<0){        // it means l is negative then only increase h
+                h++;
+            }
+            else if(h>=arr.size()){    // it means h is either last or out of bound so decrease l
+                l--;
+            }
+            else if(x-arr[l]>arr[h]-x){
+                h++;
+            }
+            else{
+                l--;
+            }
+        }
+        
+        return vector<int>(arr.begin()+l+1,arr.begin()+h);
+
+
+        // Two pointer approach - O(n-k)      - Top to bottom approach (big to small window size)
+
+        // int l=0;
+        // int h=arr.size()-1;
+
+        // while(h-l>=k){
+        //     if(x-arr[l]>arr[h]-x){
+        //         l++;
+        //     }
+        //     else{
+        //         h--;
+        //     }
+        // }
+
+        // return vector<int>(arr.begin()+l,arr.begin()+h+1);
     }
 };
