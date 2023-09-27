9665f84f3a7ae070b6c688d8efa2f915c967ecb4
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-09-27 07:32:45 +0530

    Time: 6 ms (35.24%), Space: 7.1 MB (18.09%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -5,7 +5,10 @@ public:
 
 
         // Simple method using STL functions
-        // size_t found = s.find(part);
+        // size_t found = s.find(part); // found has the index where the part is found in s
+        // cout<<found;
+
+        
         while(s.find(part) != string::npos){
             // means part exist in string s
             s.erase(s.find(part),part.length());
