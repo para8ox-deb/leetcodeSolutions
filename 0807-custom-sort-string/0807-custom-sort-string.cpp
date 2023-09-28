70da81e9f45ebe7fc61f4534f820ad387d845b3a
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-09-28 14:35:52 +0530

    Time: 4 ms (15.62%), Space: 6.6 MB (25.27%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.cpp
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -3,16 +3,16 @@ public:
 
     //custom comparator
     static string str;     // using global variable to use order string outside the function
-
     static bool compare(char c1, char c2){
 
+        // it will return true, if position of char1 in str string is less than the position of char2 in str string
+
+        
         return (str.find(c1) < str.find(c2));
     }
 
     string customSortString(string order, string s) {
         str=order;
-
-
         sort(s.begin(),s.end(),compare);    // compare is custom comparator
         return s;
     }
