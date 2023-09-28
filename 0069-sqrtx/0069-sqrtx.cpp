d1871edefc131ba0860243c89288886054386f7f
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-09-28 19:30:04 +0530

    Time: 0 ms (100%), Space: 6.2 MB (21.14%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        // It can be done using binary search in O(logn) time
+
+        int s=0;
+        int e=x;
+        
+        long long mid;
+        int ans=0;
+        while(s<=e){
+            mid=s+(e-s)/2;
+
+            if(mid*mid <= x){
+                ans=mid;
+                // move right
+                s=mid+1;
+            }
+            else{
+                //move left
+                e=mid-1;
+            }
+        }
+
+        return ans;
+    }
+};
