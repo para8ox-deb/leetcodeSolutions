87da9005e76fc140f72e789dd1b8627cceef81ad
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-09-28 19:27:23 +0530

    Time: 2 ms (57.66%), Space: 6.4 MB (7.36%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,13 +1,50 @@
 class Solution {
 public:
-    bool isPerfectSquare(int num) {
-        int res;
-        res = sqrt(num);
 
-        if(res*res==num){
-            return true;
+    int squareRootCalc(int n){
+        int start=0;
+        int end=n;
+
+        long long mid= start + (end-start)/2;
+        int ans=0;
+        while(start<=end){
+            mid = start + (end-start) / 2;
+
+            if(mid*mid > n){
+                // move left
+                end=mid-1;
+            }
+            else{
+                ans=mid;
+                start=mid+1;
+            }
         }
 
+        return ans;
+    }
+
+    bool isPerfectSquare(int num) {
+        
+        // Best approach using Binary search
+
+        int sqrt = squareRootCalc(num);
+        if(sqrt*sqrt==num)
+            return true;
+
+        
         return false;
+        
+
+
+
+        // Worst Method 
+        // int res;
+        // res = sqrt(num);
+
+        // if(res*res==num){
+        //     return true;
+        // }
+
+        // return false;
     }
 };
