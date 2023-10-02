951e1e45bbf160b9f39d4b109c8a9a6eb1837f5b
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-10-02 16:27:05 +0530

    Time: 21 ms (8.32%), Space: 20 MB (6.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // method - 1
+        // method - 1   - TC - O(n logn)
 
         sort(nums.begin(),nums.end());
         return nums[nums.size()/2];
