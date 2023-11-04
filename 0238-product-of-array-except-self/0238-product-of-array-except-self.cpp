8ce7f94fa25a1a3db05cdc990f0d6709ddfe0599
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-11-04 13:31:45 +0530

    Time: 19 ms (54.47%), Space: 25.4 MB (22.04%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res;
+        int n=nums.size();
+        int mul=1;
+        int zero=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0)
+                mul = mul*nums[i];
+            else{
+                zero++;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            // handling when zero is there
+            int val;
+            if(zero>1){
+                res.push_back(0);
+
+            }
+            else if(zero==1){
+                if(nums[i]==0){
+                    res.push_back(mul);
+                }
+                else{
+                    res.push_back(0);
+                }
+            }
+            else{
+                if(nums[i]!=0){
+                    val = mul/nums[i];
+                } 
+
+                res.push_back(val);
+            }
+           
+        }
+
+        return res;
+    }
+};
