02d21a1cffcbb9da446bc3286743523d173a8e21
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-11-06 16:05:46 +0530

    Time: 160 ms (84.36%), Space: 114.8 MB (66.32%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next != NULL){
+            fast = fast->next;
+            if(fast->next != NULL){
+                fast = fast->next;
+                slow=slow->next;
+            }
+        }
+
+        return slow;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        // / Iterative Approach
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr!=NULL){
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev=curr;
+            curr=nextNode;
+        }
+        return prev;
+    }
+
+    bool compareList(ListNode* head1, ListNode* head2){
+        while(head1!=NULL && head2!=NULL){
+            if(head1->val != head2->val){
+                return false;
+
+            }
+            else{
+                head1=head1->next;
+                head2=head2->next;
+            }
+        }
+        return true;
+    }
+
+
+    bool isPalindrome(ListNode* head) {
+        // break into 2 halves
+        // reverse the second part
+        // compare both LL
+
+        // Code to find middleNode (note - even case we need first one)
+        
+        ListNode* midNode = middleNode(head);
+        ListNode* head2 = midNode ->next;   // second or other half of LL
+        midNode->next = NULL;   // breaking the link
+
+        //after reversing the other half of LL
+        head2 = reverseList(head2);
+
+        //comparing both the LL
+        bool ans = compareList(head, head2);
+
+        return ans;
+
+    }
+};
