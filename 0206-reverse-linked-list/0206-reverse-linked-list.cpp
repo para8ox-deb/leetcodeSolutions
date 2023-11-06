89bb7068262a3bd34d1b6aba4475b7867f2ff84e
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2023-11-06 14:49:09 +0530

    Time: 7 ms (31.51%), Space: 8.7 MB (22.24%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,16 +1,44 @@
 
 class Solution {
 public:
+
+    ListNode* reverseUsingRecursion(ListNode* prev, ListNode* curr){
+        //base case
+        if(curr == NULL){
+            return prev;
+        }
+
+        //we have solved one case
+        ListNode* nextNode = curr->next;
+        curr->next = prev;
+        prev=curr;
+        curr=nextNode;
+
+        //recursion will handle this
+        ListNode* recursionKaAns = reverseUsingRecursion(prev,curr);
+        return recursionKaAns;
+    }
+
     ListNode* reverseList(ListNode* head) {
+
+        //recursive approach
+
         ListNode* prev = NULL;
         ListNode* curr = head;
 
-        while(curr!=NULL){
-            ListNode* nextNode = curr->next;
-            curr->next = prev;
-            prev=curr;
-            curr=nextNode;
-        }
-        return prev;
+        return reverseUsingRecursion(prev,curr);
+
+
+        // Iterative Approach
+        // ListNode* prev = NULL;
+        // ListNode* curr = head;
+
+        // while(curr!=NULL){
+        //     ListNode* nextNode = curr->next;
+        //     curr->next = prev;
+        //     prev=curr;
+        //     curr=nextNode;
+        // }
+        // return prev;
     }
 };
