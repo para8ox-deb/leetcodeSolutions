0ade1c9ac8ca97cea73ce8b1086aadb410739eb5
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2024-06-02 21:59:44 +0530

    Time: 4 ms (44.19%), Space: 8.8 MB (74.63%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,31 +1,55 @@
 class Solution {
 public:
     
-    void permFind(vector<int> &ds, vector<int> &nums, vector<vector<int>> &ans, int freq[]){
-        //base case 
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
+    void recurPerm(int ind, vector<int> &nums, vector<vector<int>> &ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
             return;
         }
         
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                
-                permFind(ds,nums,ans,freq);
-                freq[i]=0;
-                ds.pop_back();
-            }
+        for(int i=ind; i<nums.size(); i++){
+            swap(nums[ind], nums[i]);
+            recurPerm(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
         }
     }
     
+    
+    
+    // method-1 it takes more space as we use freq over here
+    
+//     void permFind(vector<int> &ds, vector<int> &nums, vector<vector<int>> &ans, int freq[]){
+//         //base case 
+//         if(ds.size()==nums.size()){
+//             ans.push_back(ds);
+//             return;
+//         }
+        
+//         for(int i=0;i<nums.size();i++){
+//             if(!freq[i]){
+//                 ds.push_back(nums[i]);
+//                 freq[i]=1;
+                
+//                 permFind(ds,nums,ans,freq);
+//                 freq[i]=0;
+//                 ds.pop_back();
+//             }
+//         }
+//     }
+    
     vector<vector<int>> permute(vector<int>& nums) {
+        //approach 1
+        // vector<vector<int>> ans;
+        // vector<int> ds;
+        // int freq[nums.size()];
+        // for(int i=0;i<nums.size();i++) freq[i]=0;
+        // permFind(ds,nums,ans,freq);
+        
+        
+        //approach 2 - better one
         vector<vector<int>> ans;
-        vector<int> ds;
-        int freq[nums.size()];
-        for(int i=0;i<nums.size();i++) freq[i]=0;
-        permFind(ds,nums,ans,freq);
+        recurPerm(0,nums,ans);
+        
         
         return ans;
     }
